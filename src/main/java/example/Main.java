a1f19433945b6fa10693bdbc890d4f4b71b12d38
yp <473075889>
2019-07-12 18:22:18 +0800

    1

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,15 @@
+package example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Main extends AbstractJavaFxApplicationSupport {
+
+    public static void main(String[] args) {
+        launch(Main.class, HelloworldView.class, args);
+    }
+}
